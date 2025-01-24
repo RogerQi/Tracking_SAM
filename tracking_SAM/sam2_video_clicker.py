f0e664c0e8ce244b1405fb820be896b1a20d17c7
RogerQi <rizhaoq2@illinois.edu>
2025-01-24 15:01:34 -0800

    [feat] support multi-obj tracking and negative labels

--- tracking_SAM/sam2_video_clicker.py
+++ tracking_SAM/sam2_video_clicker.py
@@ -8,6 +8,9 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import sam2
 import tempfile
+import imgviz
+
+COLORMAP = imgviz.label_colormap()
 
 class Annotator(object):
     def __init__(self, video_folder_path, sam_predictor, save_path=None):
@@ -28,26 +31,37 @@ class Annotator(object):
 
     def __gene_merge(self, r=9, cb=2, b=2, if_first=True):
         # Visualize points and countours of masks
-        pred_mask = cv2.merge([self.pred * 255, self.pred * 255, np.zeros_like(self.pred)])
-        result = np.uint8(np.clip(self.img * 0.7 + pred_mask * 0.3, 0, 255))
-        if b > 0:
-            contours, _ = cv2.findContours(self.pred, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(result, contours, -1, (255, 255, 255), b)
-        for idx, pt in enumerate(self.clicks):
-            if self.labels[idx] == 1:
-                INNER_COLOR = (0, 255, 0)
-            else:
-                INNER_COLOR = (255, 0, 0)
-            cv2.circle(result, tuple(pt), r, INNER_COLOR, -1)
-            # bright outer ring
-            cv2.circle(result, tuple(pt), r, (255, 255, 255), cb)
+        mask_viz = np.zeros(self.img.shape, dtype=np.uint8)
+        for obj_idx in self.click_label_dict:
+            mask_viz[self.pred == obj_idx] = COLORMAP[obj_idx % len(COLORMAP)]
+        result = np.uint8(np.clip(self.img * 0.7 + mask_viz * 0.3, 0, 255))
+        # if b > 0:
+        #     contours, _ = cv2.findContours(self.pred, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        #     cv2.drawContours(result, contours, -1, (255, 255, 255), b)
+        for obj_idx in self.click_label_dict:
+            for idx, pt in enumerate(self.click_label_dict[obj_idx]["clicks"]):
+                if self.click_label_dict[obj_idx]["labels"][idx] == 1:
+                    INNER_COLOR = (0, 255, 0)
+                else:
+                    INNER_COLOR = (255, 0, 0)
+                cv2.circle(result, tuple(pt), r, INNER_COLOR, -1)
+                # bright outer ring
+                cv2.circle(result, tuple(pt), r, (255, 255, 255), cb)
         return result
 
     def __reset_annotator_state(self):
-        self.clicks = np.empty([0, 2], dtype=np.int64)
-        self.labels = np.empty([0], dtype=np.int64)
+        self.click_label_dict = {}
+        self.cur_obj_idx = 0
+        self.__add_new_obj()
         self.pred = np.zeros(self.img.shape[:2], dtype=np.uint8)
         self.merge = self.__gene_merge()
+    
+    def __add_new_obj(self):
+        self.cur_obj_idx += 1
+        self.click_label_dict[self.cur_obj_idx] = {
+            'clicks': np.empty([0, 2], dtype=np.int64),
+            'labels': np.empty([0], dtype=np.int64),
+        }
 
     def __update(self):
         self.ax1.imshow(self.merge)
@@ -58,16 +72,19 @@ class Annotator(object):
         self.__update()
 
     def __predict(self):
-        # TODO(roger): support multiple instances
-        ann_obj_id = 1
+        # Add predictions to the initial frame
         _, out_obj_ids, out_mask_logits = self.sam_predictor.add_new_points_or_box(
             inference_state=self.inference_state,
             frame_idx=0,  # the frame index we interact with
-            obj_id=ann_obj_id, # give a unique id to each object we interact with (it can be any integers)
-            points=self.clicks,
-            labels=self.labels,
+            obj_id=self.cur_obj_idx, # give a unique id to each object we interact with (it can be any integers)
+            points=self.click_label_dict[self.cur_obj_idx]["clicks"],
+            labels=self.click_label_dict[self.cur_obj_idx]["labels"],
         )
-        self.pred = (out_mask_logits[0, 0] > 0).cpu().numpy().astype(np.uint8)
+        self.pred = np.zeros_like(self.pred)
+        for obj_idx in self.click_label_dict:
+            cur_mask_hw = (out_mask_logits[obj_idx - 1, 0] > 0).cpu().numpy()
+            self.pred[cur_mask_hw] = obj_idx
+            
         self.merge = self.__gene_merge()
         self.__update()
 
@@ -82,6 +99,10 @@ class Annotator(object):
             self.__reset()
         elif event.key == 'escape':
             plt.close()
+        elif event.key == ' ': # space
+            self.__add_new_obj()
+            self.fig.suptitle('[RESET]: ctrl+r; [REVOKE]: ctrl+z; [EXIT]: esc; [DONE]: enter; objIDX: {}'.format(self.cur_obj_idx), fontsize=14)
+            self.fig.canvas.draw()
         elif event.key == 'enter':
             if self.save_path is not None:
                 Image.fromarray(self.pred * 255).save(self.save_path)
@@ -90,31 +111,27 @@ class Annotator(object):
 
     def __on_button_press(self, event):
         if (event.xdata is None) or (event.ydata is None):
-            # button press is for mouse. So it should have x/y coordinates
-            raise ValueError("Invalid button press event")
+            # button press outside of image gives not xdata/ydata
+            pass
         if event.button == 1:  # 1 for left click; 3 for right click
             x, y = int(event.xdata + 0.5), int(event.ydata + 0.5)
-            self.clicks = np.append(self.clicks, np.array(
+            self.click_label_dict[self.cur_obj_idx]["clicks"] = np.append(self.click_label_dict[self.cur_obj_idx]["clicks"], np.array(
                 [[x, y]], dtype=np.int64), axis=0)
-            self.labels = np.append(self.labels, 1)
+            self.click_label_dict[self.cur_obj_idx]["labels"] = np.append(self.click_label_dict[self.cur_obj_idx]["labels"], 1)
             self.__predict()
         elif event.button == 3:
             x, y = int(event.xdata + 0.5), int(event.ydata + 0.5)
-            self.clicks = np.append(self.clicks, np.array(
+            self.click_label_dict[self.cur_obj_idx]["clicks"] = np.append(self.click_label_dict[self.cur_obj_idx]["clicks"], np.array(
                 [[x, y]], dtype=np.int64), axis=0)
-            self.labels = np.append(self.labels, 0)
+            self.click_label_dict[self.cur_obj_idx]["labels"] = np.append(self.click_label_dict[self.cur_obj_idx]["labels"], 0)
             self.__predict()
-        
 
     def main(self):
         self.fig = plt.figure('Annotator', figsize=(10, 7))
         self.fig.canvas.mpl_connect('key_press_event', self.__on_key_press)
         self.fig.canvas.mpl_connect("button_press_event", self.__on_button_press)
-        self.fig.suptitle('[RESET]: ctrl+r; [REVOKE]: ctrl+z; [EXIT]: esc; [DONE]: enter', fontsize=14)
+        self.fig.suptitle('[RESET]: ctrl+r; [REVOKE]: ctrl+z; [EXIT]: esc; [DONE]: enter; objIDX: {}'.format(self.cur_obj_idx), fontsize=14)
         self.ax1 = self.fig.add_subplot(1, 1, 1)
         self.ax1.axis('off')
         self.ax1.imshow(self.merge)
         plt.show()
-    
-    def get_mask(self):
-        return self.pred
